c23b528b4a74aa4eed9c646661de48c7c4ef3db4
Laiz Carvalho Zanlucas <104013116+zanlucaslaiz@users.noreply.github.com>
2023-10-03 09:59:53 -0300

    Update SingletonLazy.java
    anotações durante a aula

--- src/one/digitalinnovation/gof/singleton/SingletonLazy.java
+++ src/one/digitalinnovation/gof/singleton/SingletonLazy.java
@@ -1,18 +1,22 @@
 package one.digitalinnovation.gof.singleton;
 
 /**
- * Singleton "pregui�oso".
+ * Singleton "preguiçoso".
  * 
  * @author falvojr
  */
 public class SingletonLazy {
-
-	private static SingletonLazy instancia;
 	
+	/*Criando uma instancia dele mesmo*/
+	private static SingletonLazy instancia;
+	/*para garantir que ninguem vai instaciar esse padrão de projeto ele deve ter um construtor privado */
 	private SingletonLazy() {
 		super();
 	}
 	
+	/*garantir que a instacia vai ser exposta para quem chamar. 
+	 * Metodo estatico
+	 */
 	public static SingletonLazy getInstancia() {
 		if (instancia == null) {
 			instancia = new SingletonLazy();
