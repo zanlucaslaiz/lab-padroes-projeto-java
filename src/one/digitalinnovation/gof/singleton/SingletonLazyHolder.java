c664706b429e8ffa520b9bddb551907910cd1a10
Laiz Carvalho Zanlucas <104013116+zanlucaslaiz@users.noreply.github.com>
2023-10-03 10:01:26 -0300

    Update SingletonLazyHolder.java
    anotações durante a aula

--- src/one/digitalinnovation/gof/singleton/SingletonLazyHolder.java
+++ src/one/digitalinnovation/gof/singleton/SingletonLazyHolder.java
@@ -6,9 +6,10 @@ package one.digitalinnovation.gof.singleton;
  * @see <a href="https://stackoverflow.com/a/24018148">Referencia</a>
  * 
  * @author falvojr
+ * recomendada em alguns caso, pois tem um consumo de memoria mais otimizado.
  */
 public class SingletonLazyHolder {
-
+	/*encapsula a intancia em uma classe estatica externa */
 	private static class InstanceHolder {
 		public static SingletonLazyHolder instancia = new SingletonLazyHolder();
 	}
